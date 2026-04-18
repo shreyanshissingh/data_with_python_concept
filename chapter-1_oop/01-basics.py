0994030cfc74341b1ae589dffb65c89509583a21
shreyansh <shreyanshissingh@gmail.com>
2026-04-18 12:40:22 -0400

    Basic function call

---
+++ chapter-1_oop/01-basics.py
@@ -0,0 +1,14 @@
+class MyClass:
+
+    def func1(self,var1):
+        print(f"Welcome to func1 : {var1}")
+
+    def func2(self,var2):
+        print(f"Welcome to func2 : {var2}")
+
+var1 = "Shreyansh"
+var2 = "Shrey"
+obj_1 = MyClass()
+obj_1.func1(var1)
+obj_2 = MyClass()
+obj_2.func2(var2)
